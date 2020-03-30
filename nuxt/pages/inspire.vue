980a72c009e33552e2cbd645e18cba2716605f1c
Yuto Ono <stellarwindmaster@gmail.com>
2020-03-30 09:13:16 +0900

    わっしょい

--- nuxt/pages/inspire.vue
+++ nuxt/pages/inspire.vue
@@ -7,10 +7,10 @@
         class="mb-5"
       >
       <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
+        &#8220;First, say Wasshoi ╰( ‘ω’ )╯ Then, say Wasshoi ╰( ‘ω’ )╯ &#8221;
         <footer>
           <small>
-            <em>&mdash;John Johnson</em>
+            <em>&mdash;Yuto Ono</em>
           </small>
         </footer>
       </blockquote>
